18e88cff51a9941ae85e8f307003897e7dd06444
Richard-Donkor <129077887+Richard-Donkor@users.noreply.github.com>
2023-04-03 15:44:19 +0000

    Add files via upload

---
+++ RICHARD DONKOR-10960802-MTEN/Text speech - 10960802.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+import pyttsx3
+
+ttsENGINE = pyttsx3.init()
+Voice_type = ttsENGINE.getProperty('voices')
+
+layout = [[sg.Text('Select the type of voice:', text_color='black', background_color='green'),
+           sg.Radio('Male', 'RADIO1', default=True, key='Male', background_color='blue'),
+           sg.Radio('Female', 'RADIO1', key='female', background_color='blue')],
+          [sg.Text('Enter text to speak:', text_color='white', background_color='orange')],
+          [sg.InputText(key='input'), sg.Button('speak', button_color='red')],
+          ]
+
+window = sg.Window('TEXT TO SPEECH', layout, background_color='violet')
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+    elif event == 'speak':
+        text = values['input']
+        if values['Male']:
+            ttsENGINE.setProperty('voice', Voice_type[0].id)
+        elif values['female']:
+            ttsENGINE.setProperty('voice', Voice_type[1].id)
+        ttsENGINE.say(text)
+        ttsENGINE.runAndWait()
+
+window.close()
